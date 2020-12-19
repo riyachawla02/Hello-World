d4931fbd29fec750f21cc16eaa8c7e5a75a5121d
Riya Chawla <76248789+riyachawla02@users.noreply.github.com>
2020-12-19 16:44:22 +0530

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,13 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-//cout<<"hello world";
-int a,b,c;
-
-cout<<"enter 2 no"<<ends;
-cin>>a>>b;
-c=a+b;
-cout<<"sum is "<<c<<endl;
-	return 0;
+cout<<"hello world";
+return 0;
 }
 
